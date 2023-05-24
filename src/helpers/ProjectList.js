25e460197db45bab17778c9b9a576093b7f12c37
moabmo <58456373+moabmo@users.noreply.github.com>
2023-05-24 21:17:39 +0300

    Add files via upload

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,48 +1,49 @@
 import proj1 from '../assets/proj1.jpg';
+import proj2 from '../assets/proj2.jpg';
 
 export const ProjectList =[
     {
-        name: "Checkin Application",
+        name: "HELB Mobile application",
         image: proj1,
         skills: "JavaScript, PhP, CSS, Python",
     },
     {
-        name: "Checkin Application",
+        name: "HELB USSD Application",
+        image: proj2,
+        skills: "JavaScript, PhP, CSS, Python",
+    },
+    {
+        name: "HELB Servicedesk Application",
         image: proj1,
         skills: "JavaScript, PhP, CSS, Python",
     },
     {
-        name: "Checkin Application",
+        name: "Abiud's Portifolio",
         image: proj1,
         skills: "JavaScript, PhP, CSS, Python",
     },
     {
-        name: "Checkin Application",
+        name: "Checkin Application\n(2022 General Election)",
         image: proj1,
         skills: "JavaScript, PhP, CSS, Python",
     },
     {
-        name: "Checkin Application",
+        name: "KESRA Mobile Application",
         image: proj1,
         skills: "JavaScript, PhP, CSS, Python",
     },
     {
-        name: "Checkin Application",
+        name: "Intelligent Fitness Club Management System",
         image: proj1,
         skills: "JavaScript, PhP, CSS, Python",
     },
     {
-        name: "Checkin Application",
+        name: "Checkin Application\n(2019 Population Census)",
         image: proj1,
         skills: "JavaScript, PhP, CSS, Python",
     },
     {
-        name: "Checkin Application",
-        image: proj1,
-        skills: "JavaScript, PhP, CSS, Python",
-    },
-    {
-        name: "Checkin Application",
+        name: "Quiz Application",
         image: proj1,
         skills: "JavaScript, PhP, CSS, Python",
     },
