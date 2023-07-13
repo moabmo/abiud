490826b9fc91aa8328ee05549334dab974e69d45
moabmo <58456373+moabmo@users.noreply.github.com>
2023-07-13 07:27:44 +0300

    Update Articles.js

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -6,11 +6,8 @@ function Articles() {
   return (
     <>
       <div>
-        <p className='p' style={{ width: '60%', background: 'cream', border-radius: '10px', padding: '20px' }}>
-          Coming Soon...
-        </p>
         <h1>Number 13</h1>
-        <p>
+        <p className='p'>
           The number 13 has long been associated with superstitions and is considered unlucky by many people in various cultures around the world. This fear of the number, known as triskaidekaphobia, has deep historical roots and can be traced back to different origins. One popular theory is that it originates from Norse mythology, where the mischievous god Loki is said to be the 13th guest at a dinner party in Valhalla, leading to chaos and tragedy.
 
           Another explanation for the fear of the number 13 can be found in Christianity. During the Last Supper, there were 13 individuals present, including Jesus and his 12 apostles. Judas Iscariot, who later betrayed Jesus, was believed to be the 13th guest. This association with betrayal and the subsequent crucifixion of Jesus has perpetuated the negative connotations surrounding the number.
