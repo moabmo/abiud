127392ac46ec1727d1a3492e3f080d99a57e65b6
moabmo <58456373+moabmo@users.noreply.github.com>
2023-07-13 07:00:12 +0300

    Update Articles.js

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -5,6 +5,14 @@ import '../styles/Articles.css'
 function Articles() {
   return (
     <p className='p'>Coming Soon...<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></p>
+    <h1> Number 13 </h1>
+  <p>
+    The number 13 has long been associated with superstitions and is considered unlucky by many people in various cultures around the world. This fear of the number, known as triskaidekaphobia, has deep historical roots and can be traced back to different origins. One popular theory is that it originates from Norse mythology, where the mischievous god Loki is said to be the 13th guest at a dinner party in Valhalla, leading to chaos and tragedy.
+
+Another explanation for the fear of the number 13 can be found in Christianity. During the Last Supper, there were 13 individuals present, including Jesus and his 12 apostles. Judas Iscariot, who later betrayed Jesus, was believed to be the 13th guest. This association with betrayal and the subsequent crucifixion of Jesus has perpetuated the negative connotations surrounding the number.
+
+The superstition around the number 13 is further fueled by historical events and cultural beliefs. For example, in many Western cultures, it is considered unlucky to have 13 guests at a table or to begin a journey on the 13th day of the month. Buildings often omit the 13th floor, and some hotels skip room number 13 altogether. In fact, the fear of the number 13 is so prevalent that it has even influenced the naming of a phobia—triskaidekaphobia. Despite its roots in ancient mythology and religious symbolism, the superstition surrounding the number 13 continues to persist in modern society.
+    </p>
   )
 }
 
